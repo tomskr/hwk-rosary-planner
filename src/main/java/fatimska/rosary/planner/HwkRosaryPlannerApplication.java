3a7ebfe3207dd5104ccd96ddd2f61598a9d0563a
Tomasz Skrzypek <tomskr3.ts@gmail.com>
2019-07-03 19:48:19 +0200

    First Commit
    All files

---
+++ src/main/java/fatimska/rosary/planner/HwkRosaryPlannerApplication.java
@@ -0,0 +1,13 @@
+package fatimska.rosary.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwkRosaryPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwkRosaryPlannerApplication.class, args);
+    }
+
+}

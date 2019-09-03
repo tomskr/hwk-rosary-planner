552cfa1e34177095970b60404599b473dae0086a
TomaszSkrzypek <tomskr3.ts@gmail.com>
2019-09-03 22:22:41 +0200

    Added DDL and DML files

--- src/main/java/fatimska/rosary/planner/domain/Notes.java
+++ src/main/java/fatimska/rosary/planner/domain/Notes.java
@@ -7,16 +7,21 @@ import java.util.Date;
 
 @Data
 @Entity
+@Table(name="NOTES")
 public class Notes {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="NOTES_ID")
     private Long id;
 
+    @Column(name="TITLE")
     private String title;
 
     @Lob
+    @Column(name="ARTICLE")
     private String article;
 
+    @Column(name="CREA_DATE")
     private Date date;
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO NOTES (TITLE, ARTICLE, CREA_DATE) VALUES ('Hello', 'This is Hello World Article', '2008-10-29');
